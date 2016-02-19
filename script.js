f1cc51f1f9520f13cbb822ae33513b6c5d5bcbc8
Lindsay <lpeters322@gmail.com>
2016-02-19 09:46:15 -0800

    Rectangle and Line Addition

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 var canvas = d3.select("body")
               .append("svg")
-              .attr("width", 500)
-              .attr("height", 500);
+              .attr("width", 600)
+              .attr("height", 600);
+
 var circle = canvas.append("circle")
               .attr("cx", 250)
               .attr("cy", 250)
               .attr("r", 50)
               .attr("fill", "red");
+
+var rect = canvas.append("rect")
+            .attr("width", 250)
+            .attr("height", 100);
+
+var line = canvas.append("line")
+            .attr("x1", 0)
+            .attr("y1", 100)
+            .attr("x2", 400)
+            .attr("y2", 400)
+            .attr("stroke", "blue")
+            .attr("stroke-width", 5);
